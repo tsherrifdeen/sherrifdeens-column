78ca9a1eb57e86c4bcb02a899b111fc848dbd393
tsherrifdeen <tsherrifdeen@gmail.com>
2024-01-31 19:21:07 +0100

    added some seo

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+useSeoMeta({
+  title: "Sherrifdeen's Column - Home",
+  ogTitle: "Sherrifdeen's Column - Home",
+  description: 'Welcome to my amazing website for random posts and ideas.',
+  ogDescription: 'Welcome to my amazing website for random posts and ideas.',
+  ogImage: '@/assets/images/favicon-32x32.png',
+  twitterCard: '@/assets/images/favicon-32x32.png',
+})
+</script>
+
 <template>
   <NuxtLoadingIndicator />
   <NuxtPage />
